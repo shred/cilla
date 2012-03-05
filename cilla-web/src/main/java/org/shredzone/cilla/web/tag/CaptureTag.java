08dc12f2c097871829c0c59fd206d185c47a52ac
Richard Körber <github@shredzone.org>
2012-03-05 01:11:51 +0100

    Added cilla:capture tag

---
+++ cilla-web/src/main/java/org/shredzone/cilla/web/tag/CaptureTag.java
@@ -0,0 +1,61 @@
+/*
+ * cilla - Blog Management System
+ *
+ * Copyright (C) 2012 Richard "Shred" Körber
+ *   http://cilla.shredzone.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.shredzone.cilla.web.tag;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.BodyTag;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.shredzone.cilla.web.util.TagUtils;
+import org.shredzone.jshred.spring.taglib.annotation.Tag;
+import org.shredzone.jshred.spring.taglib.annotation.TagInfo;
+import org.shredzone.jshred.spring.taglib.annotation.TagParameter;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Captures the body to a variable.
+ *
+ * @author Richard "Shred" Körber
+ */
+@Tag(type = BodyTag.class)
+@TagInfo("Captures the body to a variable.")
+@Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class CaptureTag extends BodyTagSupport {
+    private static final long serialVersionUID = 6615139385480183801L;
+
+    private @TagParameter(required = true) String var;
+    private @TagParameter String scope;
+
+    @Override
+    public int doStartTag() throws JspException {
+        return EVAL_BODY_BUFFERED;
+    }
+
+    @Override
+    public int doEndTag() throws JspException {
+        String body = getBodyContent().getString();
+        TagUtils.setScopedAttribute(pageContext, var, body, scope);
+        return EVAL_PAGE;
+    }
+
+}
