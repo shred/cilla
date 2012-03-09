3a7a6764dc02339805b6f3a3d868050b6cefd598
Richard Körber <github@shredzone.org>
2012-03-09 01:00:41 +0100

    Paginator is now a fragment

---
+++ cilla-web/src/main/java/org/shredzone/cilla/web/renderer/PaginatorFragmentRenderer.java
@@ -0,0 +1,123 @@
+/*
+ * cilla - Blog Management System
+ *
+ * Copyright (C) 2012 Richard "Shred" Körber
+ *   http://cilla.shredzone.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.shredzone.cilla.web.renderer;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspWriter;
+
+import org.shredzone.cilla.service.link.LinkBuilder;
+import org.shredzone.cilla.service.link.LinkService;
+import org.shredzone.cilla.service.search.FilterModel;
+import org.shredzone.cilla.service.search.PaginatorModel;
+import org.shredzone.cilla.web.fragment.annotation.Fragment;
+import org.shredzone.cilla.web.fragment.annotation.FragmentRenderer;
+import org.shredzone.cilla.web.fragment.annotation.FragmentValue;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.HtmlUtils;
+
+/**
+ * A fragment renderer for paginators. Requires a bean that implements the
+ * {@link PaginatorRendererStrategy}.
+ *
+ * @author Richard "Shred" Körber
+ */
+@Component
+@FragmentRenderer
+public class PaginatorFragmentRenderer {
+
+    private @Resource ApplicationContext applicationContext;
+    private @Resource LinkService linkService;
+
+    @Fragment(name = "paginator")
+    public void paginatorFragment(
+        PaginatorModel model,
+        @FragmentValue("#filter") FilterModel filter,
+        Locale locale,
+        HttpServletResponse resp,
+        JspWriter out
+    ) throws IOException {
+        int selected = model.getSelectedPage();
+
+        // Setup the PaginatorStrategy
+        PaginatorRendererStrategy strategy = getPaginatorStrategy();
+        strategy.setLocale(locale);
+
+        // Open the paginator container
+        strategy.openContainer(out);
+
+        // "previous" link
+        if (model.isFirstPage()) {
+            strategy.previousLink(out, null);
+        } else {
+            strategy.previousLink(out, getUrl(resp, filter, selected - 1));
+        }
+
+        // page links
+        int current = 0, last;
+        do {
+            strategy.pageLink(out, getUrl(resp, filter, current), current, selected);
+            last = current;
+            current = strategy.computeNextPage(model, current);
+        } while (current >= 0 && current != last);
+
+        // "next" link
+        if (model.isLastPage()) {
+            strategy.nextLink(out, null);
+        } else {
+            strategy.nextLink(out, getUrl(resp, filter, selected + 1));
+        }
+
+        // Close the paginator container
+        strategy.closeContainer(out);
+        out.println();
+    };
+
+    /**
+     * Creates the URL to a page of the paginator.
+     *
+     * @param resp
+     *            {@link HttpServletResponse} for creating the link
+     * @param filter
+     *            {@link FilterModel} for the contents
+     * @param page
+     *            page number
+     * @return URL of the page
+     */
+    private String getUrl(HttpServletResponse resp, FilterModel filter, int page) {
+        LinkBuilder lb = linkService.linkTo().ref(filter);
+        if (page != 0) {
+            lb.query("p", String.valueOf(page));
+        }
+        return HtmlUtils.htmlEscape(resp.encodeURL(lb.toString()));
+    }
+
+    /**
+     * Gets a new {@link PaginatorRendererStrategy}.
+     */
+    protected PaginatorRendererStrategy getPaginatorStrategy() {
+        return applicationContext.getBean(PaginatorRendererStrategy.class);
+    }
+
+}
---
+++ cilla-web/src/main/java/org/shredzone/cilla/web/renderer/PaginatorRendererStrategy.java
@@ -0,0 +1,105 @@
+/*
+ * cilla - Blog Management System
+ *
+ * Copyright (C) 2012 Richard "Shred" Körber
+ *   http://cilla.shredzone.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.shredzone.cilla.web.renderer;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.shredzone.cilla.service.search.PaginatorModel;
+
+/**
+ * For the paginator to work, a single Spring bean must implement
+ * {@link PaginatorRendererStrategy}. Its task is to create the proper HTML output for the
+ * paginator.
+ * <p>
+ * <em>NOTE:</em> {@link PaginatorRendererStrategy} implementations must be prototype scoped!
+ *
+ * @author Richard "Shred" Körber
+ */
+public interface PaginatorRendererStrategy {
+
+    /**
+     * Sets the {@link Locale} to be used in this paginator.
+     */
+    void setLocale(Locale locale);
+
+    /**
+     * Computes the next page number to be shown.
+     *
+     * @param model
+     *            {@link PaginatorModel} with information about the paginator
+     * @param current
+     *            current page number to calculate the next number for (starting with 0)
+     * @return next page number. If {@code current} or -1 is returned, the paginator
+     *         sequence is stopped
+     */
+    int computeNextPage(PaginatorModel model, int current);
+
+    /**
+     * Opens the HTML container of the paginator.
+     *
+     * @param out
+     *            Target to write to
+     */
+    void openContainer(Appendable out) throws IOException;
+
+    /**
+     * Closes the HTML container of the paginator.
+     *
+     * @param out
+     *            Target to write to
+     */
+    void closeContainer(Appendable out) throws IOException;
+
+    /**
+     * Writes the "previous" link that jumps to the previous page.
+     *
+     * @param out
+     *            Target to write to
+     * @param url
+     *            Link to the previous page, or {@code null} if there is no previous page
+     */
+    void previousLink(Appendable out, String url) throws IOException;
+
+    /**
+     * Writes the "next" link that jumps to the next page.
+     *
+     * @param out
+     *            Target to write to
+     * @param url
+     *            Link to the next page, or {@code null} if there is no next page
+     */
+    void nextLink(Appendable out, String url) throws IOException;
+
+    /**
+     * Writes a link to a page.
+     *
+     * @param out
+     *            Target to write to
+     * @param url
+     *            Link to the current page
+     * @param current
+     *            Current page number (starting from 0)
+     * @param selected
+     *            Selected page number (starting from 0)
+     */
+    void pageLink(Appendable out, String url, int current, int selected) throws IOException;
+
+}
